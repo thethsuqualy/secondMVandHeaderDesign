c162b1ea4958dc998068455b583787340a60218a
thethsuqualy <thethsukhin.qualymm@gmail.com>
2025-05-27 16:59:51 +0630

    change hamburger menu

--- assets/js/common.js
+++ assets/js/common.js
@@ -3,8 +3,18 @@ document.addEventListener("DOMContentLoaded", () => {});
 
 // Your jQuery code goes here
 $(function () {
-  // Your code here
-  //   $("button").on("click", () => {
-  //     console.log("Hello");
-  //   });
+  const hamburger = document.getElementById("hamburger");
+  const navMenu = document.getElementById("menu");
+  const body = document.querySelector('body');
+
+
+  hamburger.addEventListener("click", function () {
+    this.classList.toggle("active"); 
+    menu.classList.toggle("show"); 
+    if(this.classList.contains("active")) {
+      body.classList.add("over-hidden");
+    } else {
+      body.classList.remove("over-hidden");
+    }
+  });
 });
