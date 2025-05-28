3ffcb7716f1c7d855004e094badc73f09bddfbb4
thethsuqualy <thethsukhin.qualymm@gmail.com>
2025-05-28 16:42:47 +0630

    add js and jQuery

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,20 +1,79 @@
 // Your JavaScript code goes here
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  // const hamburger = document.getElementById("hamburger");
+  // const menu = document.getElementById("menu");
+  // const body = document.querySelector("body");
+  // hamburger.addEventListener("click", function () {
+  //   this.classList.toggle("active");
+  //   menu.classList.toggle("show");
+  //   if (this.classList.contains("active")) {
+  //     body.classList.add("over-hidden");
+  //   } else {
+  //     body.classList.remove("over-hidden");
+  //   }
+  // });
+  // Header end
+
+
+  // tab setart
+  // const btns = document.querySelectorAll(".notice-button ");
+  // btns.forEach(function(btn) {
+  //   btn.addEventListener("click", function(e) {
+  //     e.preventDefault();
+  //     let filter = this.getAttribute("data-btn");
+  //     btns.forEach(function(btn) {
+  //       btn.classList.remove("active");
+  //     });
+  //     this.classList.add("active");
+  //     document.querySelectorAll(".content-container").forEach(function(content) {
+  //       content.classList.remove("show");
+  //     })
+  //     const matchBox = document.querySelector('.content-container[data-content="' + filter + '"]');
+  //      matchBox.classList.add('show');
+  //   })
+  // });
+  // tab end
+
+  // const btns = document.querySelectorAll(".notice-button");
+  // btns.forEach(function (btn) {
+  //   btn.addEventListener("click", function (event) {
+  //     event.preventDefault();
+  //     let filter = this.getAttribute("data-btn");
+  //     btns.forEach(function (btn) {
+  //       btn.classList.remove("active");
+  //     });
+  //     btn.classList.add("active");
+  //     const contents = document.querySelectorAll(".content-container");
+  //     contents.forEach(function (content) {
+  //       content.classList.remove("show");
+  //     });
+  //     document.querySelector('.content-container[data-content="' + filter + '"]').classList.add("show");
+  //   });
+  // });
+ 
+});
 
 // Your jQuery code goes here
 $(function () {
-  const hamburger = document.getElementById("hamburger");
-  const navMenu = document.getElementById("menu");
-  const body = document.querySelector('body');
+  $("#hamburger").click(function () {
+    $(this).toggleClass("active");
+    $("#menu").toggleClass("show");
 
-
-  hamburger.addEventListener("click", function () {
-    this.classList.toggle("active"); 
-    menu.classList.toggle("show"); 
-    if(this.classList.contains("active")) {
-      body.classList.add("over-hidden");
+    if ($(this).hasClass("active")) {
+      $("body").addClass("over-hidden");
     } else {
-      body.classList.remove("over-hidden");
+      $("body").removeClass("over-hidden");
     }
+  });
+
+  $(".notice-button").click(function (event) {
+    event.preventDefault();
+    let filter = $(this).data("btn");
+
+    $(".notice-button").removeClass("active");
+    $(this).addClass("active");
+
+    $(".content-container").removeClass("show");
+    $('.content-container[data-content="' + filter + '"]').addClass("show");
   });
 });
